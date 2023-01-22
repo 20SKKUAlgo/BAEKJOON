fee1c48b5b9c7eefaf8bc0242549bcede58ce346
ssu-on <80696703+ssu-on@users.noreply.github.com>
2023-01-22 17:20:16 +0900

    Update 230121-2108.py

--- SuYeon/230121-2108.py
+++ SuYeon/230121-2108.py
@@ -35,12 +35,14 @@ print(maxlist[1])
 print(nlist[num-1]-nlist[0])
 
 ######################################################################
-#아래 알고리즘은 런타임 에러가 뜸.... 왜......????? 답은 나옴...
+#아래 알고리즘으로 제출한 결과 맞음
+#불필요한 연산식을 최대한 배제하였음
 import sys
 import math
 input = sys.stdin.readline
 
 num = int(input())
+keyoriginal = [] #입력받은 대로 담을 리스트
 nlist = {} #입력받은 수와 그 수의 개수를 저장할 dictionary
 
 for i in range(num):
@@ -49,6 +51,7 @@ for i in range(num):
         nlist[n] = 1
     else:
         nlist[n] += 1
+    keyoriginal.append(n)
 
 keylist = list(nlist.keys()) #key만을 저장할 리스트
 valuelist = list(nlist.values()) #value형만 저장할 리스트
@@ -69,14 +72,15 @@ for i in final:
     mode.append(keylist[i])
 
 mode.sort()
-modenum = 0
-if len(mode) == 1:
+modenum = 0 #세번째 출력을 담을 변수
+if len(mode) == 1: #최빈값이 여러개가 나오지 않을 때
     modenum = mode[0]
-else:
+else: #최빈값이 여러개가 나올 때, 최빈값 중 두번째로 작은 값을 modenum 저장
     modenum = mode[1]
 
 keylist.sort()
-print(math.ceil(sum(keylist)/num))
-print(keylist[int(num/2)])
+keyoriginal.sort()
+print(round(sum(keyoriginal)/num))
+print(keyoriginal[int(num/2)])
 print(modenum)
-print(keylist[-1]-keylist[0])
+print(keyoriginal[-1]-keyoriginal[0])
