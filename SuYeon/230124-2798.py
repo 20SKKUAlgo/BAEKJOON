8a8bfabeabe9c69689f3b30f70e1c526fe13e974
ssu-on <80696703+ssu-on@users.noreply.github.com>
2023-01-24 17:25:14 +0900

    Create 230124-2798.py

---
+++ SuYeon/230124-2798.py
@@ -0,0 +1,38 @@
+#######백준 런타임 에러 나는 경우#######
+#1. 배열 인덱스 범위를 벗어났을 경우
+#2. 0으로 나눌 때
+#3. 사용하는 라이브러리에서 예외를 발생시켰을 때
+#4. 재귀 호출이 너무 깊어질 때
+
+#4번 때문에 런타임 에러가 난 것 같음... 다른 tool로 실행했을 때 답은 맞게 출력됨
+
+import sys
+input = sys.stdin.readline
+
+N, M = map(int,input().split())
+
+card = list(map(int,input().split()))
+
+card.sort() #카드리스트 정렬
+
+#앞에서부터 2개 더한 후 목표 값과의 차이가 나는 수가 있는지 확인
+#없으면 M의 값을 하나씩 빼면서 M과 합이 맞다면 출력함
+
+def find(M):   
+    for i in range(N-1):
+        stop = 0
+        for j in range(i+1,N):
+            two = card[i]+card[j]
+            dis = M - two
+            if (dis!= card[i]) and (dis!=card[j]) and (dis in card):
+                print(M)
+                stop = 1
+                break
+            else:
+                continue
+        if stop == 1:
+            break
+        if i == N-2:
+            find(M-1)
+
+find(M)
